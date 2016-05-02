1055911a8ae6016e8b31546690a981e538fcc673
solverjt <solverjt@gmail.com>
2016-05-02 16:30:24 -0600

    -- Stable Version -- (Correction)
    	The changes to main.py weren't added in the last commit,
    	this version now does.

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-
 from __future__ import print_function
 import httplib2
 import os
-import sys
 
 from apiclient import discovery
+from apiclient.http import MediaFileUpload
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
 
-#Future: Migrate from Drive v2 to v3
 
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
-SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.metadata']
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'ResumeLoader'
 
@@ -49,79 +47,88 @@ def get_credentials(flags):
     return credentials
 
 
-def find_folder(service,q):
-    results = service.files().list(
-        pageSize=1, q = q, fields="nextPageToken, files(id, name)").execute()
-    results = results.get('files',[])
-    if len(results) == 1:
-        return results[0]
-    else:
-        return None
+#currently works with:
+#   * name
+#   * parent (one parent)
+#   * mimeType
+#   * trashed
+def construct_querystring(props):
+    q = None
+    if type(props) is dict:
+        q = ''
+        trashed_specified = False
+        for prop in props:
+            q += ' and '
+            if prop == 'parents':
+                q += '"'+str(props[prop][0])+'" in '+str(prop)
+            elif prop == 'trashed':
+                q += str(props[prop])+' = '+str(prop)
+                trashed_specified = True
+            elif prop == 'name' or prop == 'mimeType':
+                q += str(prop)+' = "'+str(props[prop])+'"'
+            else:
+                q = q[:len(q)-5]
+        if not trashed_specified:
+            q += ' and trashed = False'
+        q = q[5:]
+    print(q)
+    return q
 
-#Future: Identify if the query produces only 1 result, if it does then construct a
-#           folder that adopts all characteristics of the query
-'''
-def produce_folder(service,name,q):
-    results = service.files().list(
-        pageSize=1, q = q, fields="nextPageToken, files(id, name)").execute()
-    results = results.get('files',[])
-    if len(results) == 0:
-        return results[0]
+
+def find_file(service,file_metadata):
+    file_target = None
+
+    q = construct_querystring(file_metadata)
+    if q:
+        results = service.files().list(pageSize=1, q=q, fields='files(id)').execute().get('files')
+        if len(results) == 1:
+            file_target = str(results[0]['id'])
+
+    return file_target
+
+
+#produce a file that matches file_metadata (search)
+#   create the file if file_metadata_template is specified or there isn't a result and the file_metadata is a folder
+def produce_file(service,file_metadata,file_metadata_template=None):
+    file_target = None
+    created = False
+
+    if file_metadata_template:
+        file_template = find_file(service,file_metadata_template)
+        if file_template:
+            file_target = str(service.files().copy(fileId=file_template,body=file_metadata,fields='id').execute().get('id'))
+        else:
+            try:
+                file_upload = MediaFileUpload(file_metadata_template)
+                file_target = str(service.files().create(body=file_metadata, media_body=file_upload, fields='id').execute().get('id'))
+            except Error:
+                print('Could not find template: '+str(file_metadata_template))
+                raise
+        created = True
     else:
-        return None
-'''
+        file_target = find_file(service,file_metadata)
+    if not file_target and file_metadata['mimeType'] == MIMETYPE_FOLDER:
+        file_target = str(service.files().create(body=file_metadata, fields='id').execute().get('id'))
+        created = True
+
+    return (file_target,created)
+
+#create a new folder in the folder identified by folder_metadata that...
+#   creates a folder with the 
+def cascade(service,folder_metadata):
+    '''TODO'''
+    pass
+
+
+
+
 
 MIMETYPE_FOLDER = 'application/vnd.google-apps.folder'
-MIMETYPE_FILE = 'application/vnd.google-apps.file'
+MIMETYPE_FILE = 'application/vnd.google-apps.document'
 
 FOLDER_NAME_JOBAPPLICATIONS = "Job Applications"
 FILE_NAME_RESUME = "Resume"
-FILE_NAME_COVERLETTER = "CoverLetter"
-
-def retrieve_folder_JobApplications(service):
-    # find folder Job Applications
-    query_folder_jobapplications = \
-        'name = "'+FOLDER_NAME_JOBAPPLICATIONS+'" and ' + \
-        'mimeType = "'+MIMETYPE_FOLDER+'"'
-    folder = find_folder(service,query_folder_jobapplications)
-    if not folder:
-        folder_metadata = {
-            'name' : FOLDER_NAME_JOBAPPLICATIONS,
-            'mimeType' : MIMETYPE_FOLDER
-        }
-        folder = service.files().create(body=folder_metadata, fields='id').execute().get('id')
-    else:
-        folder = folder['id']
-
-    folder_jobapplications = str(folder)
-
-    return folder_jobapplications
-
-def retrieve_files_templates(service,folder_jobapplications):
-    # find the template files Cover Letter and Resume in Job Applications
-    query_files_templates = \
-        '"'+folder_jobapplications+'" in parents and ' + \
-        '(name = "'+FILE_NAME_RESUME+'" or ' + \
-        'name = "'+FILE_NAME_COVERLETTER+'")'
-    files = service.files().list(
-        pageSize=2, q=query_files_templates, fields="files(id, name)").execute()
-    files_templates = files.get('files', [])
-
-    file_resume = None
-    file_coverletter = None
-    if files_templates[0]['name'] == FILE_NAME_RESUME:
-        file_resume = files_templates[0]['id']
-        file_coverletter = files_templates[1]['id']
-    else:
-        file_resume = files_templates[1]['id']
-        file_coverletter = files_templates[0]['id']
-
-    file_resume = str(file_resume)
-    file_coverletter = str(file_coverletter)
-
-    return {'resume':file_resume,'coverletter':file_coverletter}
-
-
+FILE_NAME_COVERLETTER = "Cover Letter"
 
 def main(flags,company_name,position_name,description):
 
@@ -133,37 +140,73 @@ def main(flags,company_name,position_name,description):
 
     ##
 
-    folder_jobapplications = retrieve_folder_JobApplications(service)
+    folder_metadata_jobapplications = {
+        'name' : FOLDER_NAME_JOBAPPLICATIONS,
+        'mimeType' : MIMETYPE_FOLDER,
+    }
+    folder_jobapplications,created = produce_file(service,folder_metadata_jobapplications)
     print('Job Applications Folder: \n\t'+folder_jobapplications)
 
-    files_templates = retrieve_files_templates(service,folder_jobapplications)
-    print('Template CoverLetter and Resume Ids: \n\t'+files_templates['resume']+'\n\t'+files_templates['coverletter'])
-
-    #find out if company name already exists, if not then create it and store info
-    company_folder_id = None
-    q_company = "name = '"+company_name+"'"
-    folder = find_folder(service,q_company)
-    if not folder:
-        folder_metadata = {
-            'name' : company_name,
-            'mimeType' : 'application/vnd.google-apps.folder',
-            'parents' : [ ja_folder_id ]
-        }
-        company_folder_id = service.files().create(body=folder_metadata, fields='id').execute().get('id')
-    else:
-        company_folder_id = folder['id']
-    print('Company Folder ID: '+str(company_folder_id))
-    #create copies of template files and move them to new Position folder
-    folder_metadata = {
-        'name' : position_name,
-        'mimeType' : 'application/vnd.google-apps.folder',
-        'parents' : [ company_folder_id ],
-        'description' : str(argv[2])
+    folder_metadata_company = {
+        'name' : company_name,
+        'mimeType' : MIMETYPE_FOLDER,
+        'parents' : [ folder_jobapplications ],
+        'properties': { 'ResumeLoaderTag': 'company' },
     }
-    position_folder_id = service.files().create(body=folder_metadata, fields='id').execute().get('id')
-    service.files().copy(fileId=cvres_ids[0]['id'],body={'parents' : [str(position_folder_id)]}).execute()
-    service.files().copy(fileId=cvres_ids[1]['id'],body={'parents' : [str(position_folder_id)]}).execute()
-    service.files().copy(fileId=cvres_ids[2]['id'],body={'parents' : [str(position_folder_id)]}).execute()
+    folder_company,created = produce_file(service,folder_metadata_company)
+    print('Company Folder: '+folder_company)
+
+    folder_metadata_position = {
+        'name' : position_name,
+        'mimeType' : MIMETYPE_FOLDER,
+        'parents' : [ folder_company ],
+        'properties': { 'ResumeLoaderTag': 'position' },
+    }
+    folder_position,created = produce_file(service,folder_metadata_position)
+    if not created:
+        print('Error: Position already exists!!!')
+        raise
+    print('Position Folder: '+folder_position)
+
+    ##
+
+    file_metadata_resume = {
+        'name' : FILE_NAME_RESUME,
+        'mimeType' : MIMETYPE_FILE,
+        'parents' : [ folder_position ],
+    }
+    file_metadata_template_resume = {
+        'name' : FILE_NAME_RESUME,
+        'mimeType' : MIMETYPE_FILE,
+        'parents' : [ folder_jobapplications ],
+    }
+    
+    q = construct_querystring(file_metadata_template_resume)
+    results = service.files().list(pageSize=1, q=q, fields='files(mimeType)').execute().get('files')
+    print('MIMETYPE: '+str(results[0]['mimeType']))
+
+
+    file_template_resume = produce_file(service,file_metadata_resume,file_metadata_template_resume)
+
+
+    file_metadata_coverletter = {
+        'name' : FILE_NAME_COVERLETTER,
+        'mimeType' : MIMETYPE_FILE,
+        'parents' : [ folder_position ],
+    }
+    file_metadata_template_coverletter = {
+        'name' : FILE_NAME_COVERLETTER,
+        'mimeType' : MIMETYPE_FILE,
+        'parents' : [ folder_jobapplications ],
+    }
+    file_template_coverletter = produce_file(service,file_metadata_coverletter,file_metadata_template_coverletter)
+
+    file_metadata_description = {
+        'name' : 'Description',
+        'mimeType' : MIMETYPE_FILE,
+        'parents' : [ folder_position ],
+    }
+    file_template_coverletter = produce_file(service,file_metadata_description,'empty.txt')
 
 
 
